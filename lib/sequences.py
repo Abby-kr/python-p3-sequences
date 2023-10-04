5c32397e5f9539bf7ec9722cb95add936c56aa2f
Abby-kr <abby.a.otieno@gmail.com>
2023-10-04 16:49:50 +0300

    Completed Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    num1 = 0
+    num2 = 1
+    sum = 0
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_list = [0,1]
+        for i in range(2,length):
+            sum = num1 + num2
+            num1 = num2
+            num2 = sum
+            fibonacci_list.append(sum)
+        print(fibonacci_list)
